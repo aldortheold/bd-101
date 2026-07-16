42e8542dcfd2880cdf02af8c3030b21de8b0e769
Andrew <aldortheold@gmail.com>
2026-07-16 23:17:47 +1000

    added a basic welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "./Welcome";
 
 function App() {
 
     return (
-        <></>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Welcome />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
